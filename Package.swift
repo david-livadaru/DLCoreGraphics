a165c0210ae734f9104464a5ccdb6f64357f68dd
David <davidlivadaru@gmail.com>
2017-02-19 23:24:04 +0200

    Update .gitignore file to ignore downloaded dependencies. Add Package dependencies. Link CoreGraphicsExtension with Angle framework.

---
+++ Package.swift
@@ -0,0 +1,5 @@
+import PackageDescription
+
+let package = Package(name: "CoreGraphicsDLExtension",
+                      dependencies: [.Package(url: "https://github.com/davidlivadaru/DLAngle.git", 
+                                              majorVersion: 0)])
