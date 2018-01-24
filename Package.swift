488ade1f4d5c61a665996ac5d824779c5cde4538
David <davidlivadaru@gmail.com>
2018-01-24 11:03:47 +0200

    develop
    Update Swift Package to version.
    Create separate project for each OS.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
-let package = Package(name: "CoreGraphicsDLExtension",
-                      dependencies: [.Package(url: "https://github.com/davidlivadaru/DLAngle.git", 
-                                              majorVersion: 0)])
+let package = Package(
+    name: "DLCoreGraphics",
+    products: [
+        .library(
+            name: "DLCoreGraphics",
+            targets: ["DLCoreGraphics"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "DLCoreGraphics",
+            dependencies: []),
+        .testTarget(
+            name: "DLCoreGraphicsTests",
+            dependencies: ["DLCoreGraphics"]),
+    ]
+)
---
+++ tvOS/DLCoreGraphics/DLCoreGraphics/DLCoreGraphics.h
@@ -0,0 +1,19 @@
+//
+//  DLCoreGraphics.h
+//  DLCoreGraphics
+//
+//  Created by David Livadaru on 1/24/18.
+//  Copyright © 2018 David Livadaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DLCoreGraphics.
+FOUNDATION_EXPORT double DLCoreGraphicsVersionNumber;
+
+//! Project version string for DLCoreGraphics.
+FOUNDATION_EXPORT const unsigned char DLCoreGraphicsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DLCoreGraphics/PublicHeader.h>
+
+
